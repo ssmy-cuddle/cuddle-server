825fb62d79b9e810b7e90706e32c9d259cb6b5c0
GEPI2 <67233009+GEPI2@users.noreply.github.com>
2024-10-31 13:26:00 +0900

    Update posts.py
    User moduledelete

--- app/models/posts.py
+++ app/models/posts.py
@@ -2,13 +2,12 @@ from sqlalchemy import Column, String, Integer, TIMESTAMP, Enum, Boolean, Foreig
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from . import Base
-from user import User
 
 class Posts(Base):
     __tablename__ = "posts"
 
     post_id = Column(String(100), primary_key=True)  # 사용자가 입력한 uid
-    uid = Column(String(50), ForeignKey('user.uid'), nullable=False)  # 사용자 ID
+    uid = Column(String(50), ForeignKey('users.uid'), nullable=False)  # 사용자 ID
     created_at = Column(TIMESTAMP, server_default=func.now())  # 생성 시간
     is_deleted = Column(Boolean, nullable=False, default=False)  # 삭제 여부
     last_updated = Column(TIMESTAMP, server_default=func.now(), onupdate=func.now())  # 업데이트 시간
@@ -21,4 +20,3 @@ class Posts(Base):
     title = Column(String(300), nullable=False)  # 게시글 제목
     content = Column(String, nullable=False)  # 게시글 본문
 
-    user = relationship('User', back_populates='posts')
